994a145c4f2a1d18c1d28f38509f3074b518c445
Jonathan Missler <jonathanmissler@MacBook-Pro.fritz.box>
2020-08-24 15:01:36 +0200

    new functions added

--- datagen_funcs.R
+++ datagen_funcs.R
@@ -1,9 +1,24 @@
-install.packages('EnvStats', dependencies = TRUE)
+
 
 library(tidyverse)
-library(EnvStats)
 
 price <- function(current_price = 336){
-  new_price <- current_price + rpareto(n = 1, location = 336)
+  new_price <- current_price + rcauchy(n = 1, scale = 100, location = 336)
+  
+  if(new_price < 0){
+    new_price <- 17
+    }
+  
   return(new_price)
+}
+
+
+worker_happiness <- function(wages, infra){
+  
+  if(wages <= 0){
+    happiness <- 0
   }
+  happiness <- tanh(2*wages/100 + log(infra)/50)
+  
+  return(happiness)
+}
