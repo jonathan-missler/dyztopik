fa68582b7e3af97578ca8b4e42e4aaeb25eb3af5
Jonathan Missler <jonathanmissler@MacBook-Pro.fritz.box>
2020-10-13 16:34:27 +0200

    solved demand issue

---
+++ App/datagen_funcs.R
@@ -0,0 +1,42 @@
+
+
+price <- function(current_price = 336){
+  new_price <- current_price + rcauchy(n = 1, scale = 100, location = 0)
+  
+  if(new_price < 0){
+    new_price <- 17
+    }
+  
+  return(new_price)
+}
+
+
+worker_happiness <- function(wages, infra){
+  
+  if(wages <= 0 | infra <= 0){
+    happiness <- 0
+  } else {
+    happiness <- tanh(wages/50 + log(infra)/50)
+  }
+  return(happiness)
+}
+
+
+
+yield <- function(n_workers, tech){
+  quantity <- log(n_workers^2) * log(tech^2) + rnorm(n = 1, mean = 0, sd = 15)
+  return(quantity)
+}
+
+
+demand <- function(pers_price, price){
+  price_diff <- price - pers_price
+  
+  demanded_quant <- 50*tanh(0.05*price_diff)+350  
+
+  return(demanded_quant)
+}
+
+
+
+
